ab30b93d4ef293bad382ea1ed587be5692105728
Inna12018 <innadrabchuk@gmail.com>
2020-05-25 19:00:36 +0300

    added items

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
 $(function(){
 
+    $(".rate__star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });  
 
-    
+
+
+$(".js-range-slider").ionRangeSlider({
+  type: "double",
+  min: 0,
+  max: 1000,
+  from: 200,
+  to: 500,
+  grid: true
+});
+
+var mixer = mixitup('.products__inner-mix-box');  
 });
